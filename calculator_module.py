cca7814829508ebf595f93af0fe9f86bfec33008
Swathi E K <157112703+SwathiEK21@users.noreply.github.com>
2024-01-19 16:07:52 +0530

    Update calculator_module.py

--- calculator_module.py
+++ calculator_module.py
@@ -12,5 +12,5 @@ class Calculator:
 
     def divide(self, a, b):
         if b == 0:
-            raise ValueError("Cannot divide by zero")
+            raise ValueError("Error")
         return a / b
